edcf4a419c542cb8e52429cd1905ce0296940eaf
ajokt123 <at4933@nyu.edu>
2025-03-02 16:38:28 -0500

    changes

--- helpers/get_user.py
+++ helpers/get_user.py
@@ -2,7 +2,9 @@ import pymongo
 import os
 from datetime import datetime, timezone
 import json
+from dotenv import load_dotenv
 
+load_dotenv(override=True) 
 database_url = os.getenv('MONGO_URI')
 client = pymongo.MongoClient(database_url)
 db = client[os.getenv("MONGO_DBNAME")]

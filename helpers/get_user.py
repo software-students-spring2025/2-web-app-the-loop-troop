0ecd656a399d1274aabad33a0b193112fc497718
ajokt123 <at4933@nyu.edu>
2025-03-03 19:00:19 -0500

    fixing janedoe issue

--- helpers/get_user.py
+++ helpers/get_user.py
@@ -7,15 +7,14 @@ load_dotenv(override=True)
 database_url = os.getenv('MONGO_URI')
 client = pymongo.MongoClient(database_url)
 db = client[os.getenv("MONGO_DBNAME")]
-collection = db["Users"]
+collection = db["users"]
 
-def get_user(email):
-    query = {"email": email}
+
+def get_user(username):
+    query = {"username": username}
     user = collection.find_one(query)
-    return user
-    if user: 
+    
+    if user:
         return user
     else:
-        return {"error": "Not found sorrayyy"}
-
-
+        return {"error": "User not found."}
